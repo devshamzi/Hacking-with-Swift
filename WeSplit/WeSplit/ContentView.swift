5d5b948b58d40a7d83b74a8864cd1eea6d8a6d35
Shamzi <shamzi.erz@hotmail.com>
2021-11-17 11:21:49 +0400

    Testing

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+                    Text("Hello, world!")
+
+                Section{
+                    Group{
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                    }
+                    Group{
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                    }
+
+                    Group{
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                        Text("Hello, world!")
+                    }
+                }
+            }.navigationTitle("SwiftUI")
+                .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
